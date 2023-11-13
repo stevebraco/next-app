f11e6e8a641fcf8fcb409d2f732cd0e0e84af829
steve <steve.braco@gmail.com>
2023-11-13 10:45:52 +0000

    finish home title button

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,23 @@
 // import { UserButton } from "@clerk/nextjs";
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      Home
-      {/* <UserButton afterSignOutUrl="/" /> */}
-    </div>
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 justify-between gap-5 max-sm:flex-col sm:items-center">
+        {/* <LocalSearchBar />
+        <Filter /> */}
+      </div>
+    </>
   );
 }
