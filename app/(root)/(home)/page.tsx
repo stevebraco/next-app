5c514cebfdedfa6e4eb1f1c838f15abbb87f373c
steve <steve.braco@gmail.com>
2023-11-13 16:27:04 +0000

    finish home

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,42 @@
 // import { UserButton } from "@clerk/nextjs";
 
+import QuestionCard from "@/components/card/QuestionCard";
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/filter/Filter";
+import NoResult from "@/components/shared/noResult/NoResult";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
+const questions = [
+  {
+    _id: 1,
+    title: "Cascading Deletes in SQLAlchemy?",
+    tags: [
+      { _id: 1, name: "python" },
+      { _id: 2, name: "sql" },
+    ],
+    author: { _id: 1, name: "John Doe", picture: "john.jpg" },
+    upvotes: 101506,
+    views: 1000007,
+    answers: [],
+    createdAt: new Date("2022-12-01T12:00:00.000Z"),
+  },
+  {
+    _id: 2,
+    title: "How to center a div?",
+    tags: [
+      { _id: 3, name: "html" },
+      { _id: 4, name: "css" },
+    ],
+    author: { _id: 2, name: "Jane Smith", picture: "jane.jpg" },
+    upvotes: 35001,
+    views: 150000002,
+    answers: [],
+    createdAt: new Date("2023-09-02T10:30:00.000Z"),
+  },
+];
 export default function Home() {
   return (
     <>
@@ -15,8 +49,43 @@ export default function Home() {
         </Link>
       </div>
       <div className="mt-11 justify-between gap-5 max-sm:flex-col sm:items-center">
-        {/* <LocalSearchBar />
-        <Filter /> */}
+        <LocalSearchBar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+      <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              answers={question.answers}
+              upvotes={question.upvotes}
+              views={question.views}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a question"
+          />
+        )}
       </div>
     </>
   );
