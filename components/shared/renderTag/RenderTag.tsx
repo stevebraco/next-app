172c556f0305a124663055cf9022b10c2c51570a
steve <steve.braco@gmail.com>
2024-01-20 11:25:42 +0100

    correction

--- components/shared/renderTag/RenderTag.tsx
+++ components/shared/renderTag/RenderTag.tsx
@@ -6,7 +6,7 @@ interface Props {
   _id: number;
   name: string;
   totalQuestions?: number;
-  showCount?: number;
+  showCount?: boolean;
 }
 const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
   return (
