d50f02e3b14973201f44c3e6d127ddb2fb3ac433
steve <steve.braco@gmail.com>
2024-01-21 02:25:45 +0100

    correction deploy vercel

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/app/api/webhook/clerk",
+    "app/api/webhook/clerk",
     "/question/:id",
     "/tags",
     "/tags/:id",
@@ -14,7 +14,7 @@ export default authMiddleware({
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/app/api/webhook/clerk", "/api/chatgpt", "/"]
+  ignoredRoutes: ["app/api/webhook/clerk", "/api/chatgpt",]
 });
 
 export const config = {
